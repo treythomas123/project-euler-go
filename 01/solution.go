610b7c6a99b1ea16e59ad57478260dd7eb578fda
Trey Thomas <trey@treythomas.me>
2016-10-06 17:20:34 -0600

    Add solution to problem 1

---
+++ 01/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    max := 1000
+    if len(os.Args) > 1 {
+        maxArg, err := strconv.Atoi(os.Args[1])
+        if err == nil {
+            max = maxArg
+        }
+    }
+    fmt.Println(solution(max))
+}
+
+func solution(max int) int {
+    sum := 0
+    for i := 1; i < max; i++ {
+        if i%3 == 0 || i%5 == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
